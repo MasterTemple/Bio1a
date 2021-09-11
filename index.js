846191c2a2ed38382d3d21d3ecc7df26aa66e187
MasterTemple <dgmastertemple@protonmail.com>
2021-09-11 13:30:43 -0700

    better rejection message

--- index.js
+++ index.js
@@ -86,7 +86,26 @@ client.on('interactionCreate', async (interaction) => {
     let apiKey = await getApiKeyForUser(config, interaction.user.id)
     let allowedCommands = ["register", "help", "games", "addrole", "addMajorRole", "addYearRole"]
     if(apiKey === undefined && (!allowedCommands.includes(interaction.commandName) && !allowedCommands.includes(interaction.customId.replace(/\[[^\]]]/g, "")))){
-        interaction.reply({content: "You are not registered! Please try the `/help </register>` command to register yourself", ephemeral: true})
+        // interaction.reply({content: "You are not registered! Please try the `/help </register>` command to register yourself", ephemeral: true})
+        
+            let embed = new MessageEmbed().setColor(config.bot.color).setTitle("How To Register").setThumbnail(config.bot.iconUrl).setImage("https://cdn.discordapp.com/attachments/877643936733216788/877672323522654298/createToken.gif")
+            let settingsUrl = `https://${config.canvasDomain}/profile/settings`
+
+            embed.setDescription(`To use a Canvas related feature with this bot, you must be registered. To register your Canvas account with Bio1a, you must create an API Token.\n\nTo create an API Token, go [here](${settingsUrl}) or click the button below.\n\nOnce you are at the settings page scroll down to **Approved Integrations** and click **+ New Access Token**.\n\nIn the **Purpose:** field put something like \`Canvas Discord Bot\` then click **Generate Token**.\n\nYour token should look something like this: \`1872~3W72HdKuFbgs77kSISduWgsCwWDX4035A0ETGtC9Bef0MtY3z06GAZEpf71wu6B7\``)
+            let components = [
+                {
+                    "type": 1,
+                    "components": [
+                        {
+                            "type": 2,
+                            "label": "Create Token!",
+                            "style": 5,
+                            "url": settingsUrl
+                        }
+                    ]
+                }
+            ]
+            interaction.reply({embeds: [embed], components: components, ephemeral: true})
         return
     }
     
