8cc00f08c48b7ca6462f3e0dc5cd48a273147053
MasterTemple <dgmastertemple@protonmail.com>
2021-09-20 13:33:36 -0700

    removes roles

--- functions/selectMenus/addTeamRole.js
+++ functions/selectMenus/addTeamRole.js
@@ -1,12 +1,23 @@
 module.exports = async (interaction, config, accessToken) => {
     // console.log(interaction);
     let teams = require("./../../data/intramuralLeagues.json")
+
     let roleToAdd = interaction.values[0]
     let member = interaction.member
     // console.log(roleToAdd);
     try{
     let userRoles = [...member.roles.cache.keys()]
     let teamRoles = []
+
+    Object.values(teams).forEach((eachSport) => {
+        Object.values(eachSport.leagues).forEach((eachLeague) => {
+            // console.log(eachLeague);
+            eachLeague.forEach((eachTeam) => {
+                teamRoles.push(eachTeam.roleId)
+            })
+        })
+    })
+
     //teamRoles.forEach( eachMajorRole => {
 	for(let eachMajorRole of teamRoles){
         if(userRoles.includes(eachMajorRole)){
